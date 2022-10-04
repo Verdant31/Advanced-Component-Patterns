6b29a3780bc0d01e9c46a600aee9d853135a9dc5
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-04 15:43:46 -0300

    Completed

---
+++ showcase/src/patterns/10.js
@@ -0,0 +1,291 @@
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState, useReducer } from 'react';
+import styles from './index.css';
+import mojs from 'mo-js';
+import userCustomStyles from './usage.css'
+
+const INITIAL_CLAP_STATE = {
+  count: 0,
+  countTotal: 267
+}
+
+//Starts - Hooks
+const useClapAnimation = ({
+  clapEl,
+  countEl,
+  clapTotalEl
+}) => {
+  const [ animationTimeline, setAnimationTimeline ] = useState(() => new mojs.Timeline());
+
+  useLayoutEffect(() => {
+    if(!clapEl || !countEl || !clapTotalEl) return;
+    //Defining animation values
+    const tlDuration = 300;
+    //Defining the animation behavior
+    const scaleButton = new mojs.Html({
+      el: clapEl,
+      duration: 300,
+      scale: {1.3: 1},
+      easing: mojs.easing.ease.out,
+    })
+    const triangleBurst = new mojs.Burst({
+      parent: clapEl,
+      radius: {50 : 95},
+      count: 5,
+      angle: 30,
+      children: {
+        shape: 'polygon',
+        radius: {6 : 0},
+        stroke: 'rgba(211, 54, 0, 0.5)',
+        strokeWidth: 2,
+        angle: 210,
+        delay: 30,
+        speed: 0.2,
+        duration: tlDuration,
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
+      }
+    }); 
+    const circleBurst = new mojs.Burst({
+      parent: clapEl,
+      radius: {50 : 75},
+      count: 5,
+      angle: 25,
+      duration: tlDuration,
+      children: {
+        shape: 'circle',
+        fill: 'rgba(149, 165, 166, 0.5)',
+        delay: 30,
+        speed: 0.2,
+        radius: {3 : 0},
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
+      }
+    });
+    const countAnimation = new mojs.Html({
+      el: countEl,
+      opacity: {0: 1},
+      duration: tlDuration,
+      y: {0: -30},
+    }).then({
+      opacity: {1: 0},
+      delay: tlDuration/2,
+      y: -80,
+    })
+    const countTotalAnimation = new mojs.Html({
+      el: clapTotalEl,
+      opacity: {0: 1},
+      delay: (3 * tlDuration) / 2,
+      duration: tlDuration,
+      y: {0: -3}
+    })
+    //Resing the clap div to 1,1
+    if(typeof clapEl === 'string') {
+      const clap = document.getElementById('#clap');
+      clap.style.transform = 'scale(1,1)'
+    }else {
+      clapEl.style.transform = 'scale(1,1)'
+    }
+    const newAnimationTimeline = animationTimeline.add([scaleButton, countTotalAnimation, countAnimation, triangleBurst, circleBurst]);
+    setAnimationTimeline(newAnimationTimeline)
+  }, [clapEl, countEl, clapTotalEl])
+  return animationTimeline;
+}
+const useDOMRef = () => {
+    const [DOMRef, setRefs] = useState({})
+
+    const setRef = useCallback(
+        (node) => {
+          setRefs(oldState=> (
+            {
+              ...oldState,
+              [node.dataset.refkey]: node,
+            }
+          ))
+      },[])
+    return [DOMRef, setRef];
+}
+
+const callFnsInSequence = (...fns) => (...args) => {
+  fns.forEach(fn => fn && fn(...args));
+} 
+
+const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  })
+  return ref.current;
+}
+const MAXIMUM_USER_CLAP = 20;
+const internalReducer = ({count, countTotal}, {type, payload}) => {
+    switch(type) {
+        case 'clap': 
+            return {
+                count: Math.min(count + 1, MAXIMUM_USER_CLAP),
+                countTotal: count < MAXIMUM_USER_CLAP ? countTotal + 1 : countTotal
+            }
+        case 'reset':
+            return payload
+      default:
+            break;
+
+    }
+}
+const useClapState = (initialClapState = INITIAL_CLAP_STATE, reducer = internalReducer) => {
+    const [clapState, dispatch] = useReducer(reducer, initialClapState);
+    const userInitialState = useRef(initialClapState)
+    const { count, countTotal } = clapState;
+
+    const updateClapState = () =>  dispatch({type: 'clap'})
+
+    const resetRef = useRef(0)
+    const prevCount = usePrevious(count);
+    const reset = useCallback(() => {
+      if(prevCount !== count) {
+        dispatch({type: 'reset', payload: userInitialState.current})
+        resetRef.current++;
+      }
+    }, [prevCount, count])
+
+    const getTogglerProps = ({onClick, ...otherProps} = {}) => ({
+      onClick: callFnsInSequence(updateClapState, onClick),
+      'aria-pressed': clapState.count > 0,
+      ...otherProps
+    })
+    const getCounterProps = ({...otherProps}) => ({
+      count,
+      'aria-valuemax': MAXIMUM_USER_CLAP,
+      'aria-valuemin': 0,
+      'aria-valuenow': count,
+      ...otherProps
+    })
+    return { clapState, updateClapState, getTogglerProps, getCounterProps, reset, resetDep: resetRef.current }
+}
+
+useClapState.reducer = internalReducer;
+useClapState.types = {
+    clap: 'clap',
+    reset: 'reset'
+}
+
+const useEffectAfterMount = (cb, depArr) => {
+    const componentDidMount = useRef(true);
+    useEffect(() => {
+        if(!componentDidMount.current) {
+            return cb();
+        }
+        componentDidMount.current = false;
+    }, depArr)
+}
+//Ends - Hooks
+
+//Starts - Subcomponents
+const ClapIcon = ({count}) => {
+  return (
+    <span>
+      <svg  xmlns='http://www.w3.org/2000/svg' viewBox='-549 338 100.1 125' className={`${styles.icon} ${count > 0 && styles.checked}`}>
+        <path d='M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z' />
+        <path d='M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9' />
+      </svg>
+    </span>
+  )
+}
+
+const ClapContainer = ({children, setRef, handleClick, ...props}) => {
+    return (
+        <button ref={setRef} onClick={handleClick}  className={styles.clap} {...props}>
+            {children}
+        </button>
+    )
+}
+
+const ClapCount = ({count, setRef, ...props}) => {
+  return <span  ref={setRef} className={styles.count} {...props}>+ {count}</span>
+}
+
+const CountTotal = ({countTotal, setRef, ...props}) => {
+  return <span  ref={setRef} className={styles.total} {...props}>{countTotal}</span>
+}
+//Ends - Subcomponents
+
+//Starts - Usage of our component
+
+const USER_INITIAL_CLAP_STATE = {
+  count: 0,
+  countTotal: 40000
+}
+//Import useClapState from '...
+const Usage = () => {
+    const [timesClapped, setTimesClapped] = useState(0)
+    const clappedTooMuch = timesClapped >= 7
+    const reducer = (state, action) => {
+        if(action.type === useClapState.types.clap && clappedTooMuch) {
+            return state
+        }
+       return useClapState.reducer(state, action)
+    }
+  const {
+    clapState, 
+    getTogglerProps, 
+    getCounterProps, 
+    reset, 
+    resetDep 
+    } = useClapState(USER_INITIAL_CLAP_STATE, reducer);
+  const { count, countTotal } = clapState; 
+  const [ isUploading, setIsUploading] = useState(false)
+  const [{clapRef, clapTotalRef, clapCountRef}, setRef ] = useDOMRef();
+
+  const animationTimeline = useClapAnimation({
+    clapEl: clapRef,
+    countEl: clapCountRef,
+    clapTotalEl: clapTotalRef
+  });
+
+  const handleClick = () => {
+    setTimesClapped(t => t = t + 1)
+    console.log('asdsadsad');
+  }
+  useEffectAfterMount(() => {
+    animationTimeline.replay();
+  }, [count])
+
+  useEffectAfterMount(() => {
+    setIsUploading(true);
+    setTimesClapped(0)
+    const id = setTimeout(() => {
+      setIsUploading(false);
+    }, 3000)
+    return () => clearTimeout(id);
+  }, [resetDep])
+
+  return (
+    <div>
+    <ClapContainer  setRef={setRef} data-refkey="clapRef" {...getTogglerProps({
+      'aria-pressed': false,
+      onClick: handleClick,
+    })}>
+      <ClapIcon count={count} />    
+      <ClapCount count={count} setRef={setRef} data-refkey="clapCountRef" {...getCounterProps()} />
+      <CountTotal countTotal={countTotal} setRef={setRef} data-refkey="clapTotalRef"/>
+    </ClapContainer>
+    <section>
+      <button onClick={reset} className={userCustomStyles.resetBtn}>
+        Reset
+      </button>
+      <pre className={userCustomStyles.resetMsg}>
+        {JSON.stringify({timesClapped, count, countTotal})}
+      </pre>
+      <pre>
+        {isUploading ? 'uploading reset' + resetDep + '...' : ''}
+      </pre>
+      <pre style={{color: 'red'}}>
+        {clappedTooMuch ? 'You have clapped too much. Dont be so generous!' : ''}
+      </pre>
+    </section>
+    </div>
+  )
+}
+//Ends - Usage of our component
+
+export default Usage;
+
+
+
